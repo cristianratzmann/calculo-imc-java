574f74cf34063843f8f6fb0f053a9fa50a5cc444
Aluno <Aluno@SN-279252>
2019-02-05 19:51:08 -0200

    Criado a interface gráfica Criado a classe de calculo IMC Tratamento dos textos das labes

---
+++ src/calculoimc/Imc.java
@@ -0,0 +1,16 @@
+
+package calculoimc;
+
+import java.text.DecimalFormat;
+
+
+public class Imc {
+    
+    
+    public double calcular (double peso, double altura){
+        
+    return  peso / (altura * altura);
+   
+    }
+    
+}
